be504eb98bd1976dd11d8b9406ef50d77b3a55f4
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-09-24 15:56:18 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LetsCodeBiblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
